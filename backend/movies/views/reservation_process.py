a53ce89bb45ed9291c800d1b291879043870e0c4
Julian Korgol <me@juliankorgol.com>
2025-09-28 18:22:28 +0200

    refactor: remove unused import

--- backend/movies/views/reservation_process.py
+++ backend/movies/views/reservation_process.py
@@ -1,5 +1,3 @@
-from tkinter.scrolledtext import example
-
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import generics, status
 from rest_framework.response import Response
