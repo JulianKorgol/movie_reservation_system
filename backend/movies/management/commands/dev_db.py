b472049053f16fdc9227126475d921c5cdd74747
Julian Korgol <me@juliankorgol.com>
2025-10-08 11:21:13 +0200

    feat: add cinemas to development data

--- backend/movies/management/commands/dev_db.py
+++ backend/movies/management/commands/dev_db.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from movies.models import Country, City
+from movies.models import Country, City, Cinema
 
 
 class Command(BaseCommand):
@@ -41,5 +41,77 @@ class Command(BaseCommand):
     city_usa_second.save()
 
     # ***END Cities***
+
+    # ***Cinemas***
+    Cinema.objects.all().delete()
+
+    cinema_warsaw_first = Cinema.objects.create(
+      name="Złote Tarasy",
+      city=city_poland_first,
+      postal_code="00-001",
+      street="Złota",
+      street_number="3A",
+      url="zlote-tarasy")
+    cinema_warsaw_first.save()
+
+    cinema_warsaw_second = Cinema.objects.create(
+      name="Arkadia",
+      city=city_poland_first,
+      postal_code="00-003",
+      street="arkadiowa",
+      street_number="7A",
+      url="arkadia")
+    cinema_warsaw_second.save()
+
+    cinema_warsaw_third = Cinema.objects.create(
+      name="Mokotów",
+      city=city_poland_first,
+      postal_code="00-008",
+      street="mokotowska",
+      street_number="3A",
+      url="mokotow")
+    cinema_warsaw_third.save()
+
+    cinema_krakow_first = Cinema.objects.create(
+      name="Bonarka",
+      city=city_poland_second,
+      postal_code="30-415",
+      street="Kamieńskiego",
+      street_number="11",
+      url="bonarka"
+    )
+    cinema_krakow_first.save()
+
+    cinema_lublin_first = Cinema.objects.create(
+      name="Plaza Lublin",
+      city=city_poland_third,
+      postal_code="20-029",
+      street="Lipowa",
+      street_number="13",
+      url="plaza-lublin"
+    )
+    cinema_lublin_first.save()
+
+    cinema_newyork_first = Cinema.objects.create(
+      name="AMC Empire 25",
+      city=city_usa_first,
+      postal_code="10036",
+      street="W 42nd St",
+      street_number="234",
+      url="amc-empire-25"
+    )
+    cinema_newyork_first.save()
+
+    cinema_sandiego_first = Cinema.objects.create(
+      name="Landmark Hillcrest",
+      city=city_usa_second,
+      postal_code="92103",
+      street="University Ave",
+      street_number="3965",
+      url="landmark-hillcrest"
+    )
+    cinema_sandiego_first.save()
+
+    # ***END Cinemas***
 
     # END App required data
