faf8742f82bdff312a8fb6fd0c34d659d7ba198c
Julian Korgol <me@juliankorgol.com>
2025-10-09 17:33:55 +0200

    feat: prepare development data for showtimes endpoint

--- backend/movies/management/commands/dev_db.py
+++ backend/movies/management/commands/dev_db.py
@@ -1,6 +1,9 @@
+from decimal import Decimal
+
 from django.core.management.base import BaseCommand
 
-from movies.models import Country, City, Cinema
+from movies.models import Country, City, Cinema, Movie, MovieGenre, CinemaRoom, CinemaRoomRow, CinemaRoomSeat, \
+  TicketType, Showtime
 
 
 class Command(BaseCommand):
@@ -8,8 +11,6 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     print("Loading development data")
-
-    # App required data
 
     # ***Country***
     Country.objects.all().delete()
@@ -42,6 +43,34 @@ class Command(BaseCommand):
 
     # ***END Cities***
 
+    # ***Ticket Types***
+    TicketType.objects.all().delete()
+
+    ticket_type_poland_normal = TicketType.objects.create(
+      country=country_first,
+      name="Normal Ticket",
+      price=27.00,
+      currency="PLN"
+    )
+
+    ticket_type_poland_discounted = TicketType.objects.create(
+      country=country_first,
+      name="Discounted Ticket",
+      currency="PLN",
+      primary_ticket=ticket_type_poland_normal,
+      discount_percentage=37.00
+    )
+
+    ticket_type_poland_vip = TicketType.objects.create(
+      country=country_first,
+      name="Discounted Ticket",
+      price=Decimal("40.00"),
+      currency="PLN",
+      primary_ticket=ticket_type_poland_normal,
+    )
+
+    # ***END Ticket Types)
+
     # ***Cinemas***
     Cinema.objects.all().delete()
 
@@ -63,14 +92,14 @@ class Command(BaseCommand):
       url="arkadia")
     cinema_warsaw_second.save()
 
-    cinema_warsaw_third = Cinema.objects.create(
-      name="Mokotów",
-      city=city_poland_first,
-      postal_code="00-008",
-      street="mokotowska",
-      street_number="3A",
-      url="mokotow")
-    cinema_warsaw_third.save()
+    # cinema_warsaw_third = Cinema.objects.create(
+    #   name="Mokotów",
+    #   city=city_poland_first,
+    #   postal_code="00-008",
+    #   street="mokotowska",
+    #   street_number="3A",
+    #   url="mokotow")
+    # cinema_warsaw_third.save()
 
     cinema_krakow_first = Cinema.objects.create(
       name="Bonarka",
@@ -114,4 +143,256 @@ class Command(BaseCommand):
 
     # ***END Cinemas***
 
-    # END App required data
+    # ***Movies***
+    MovieGenre.objects.all().delete()
+    Movie.objects.all().delete()
+
+    movie_genre_thriller = MovieGenre.objects.create(name="Thriller", url="thriller")
+    movie_genre_thriller.save()
+
+    movie_genre_action = MovieGenre.objects.create(name="Action", url="action")
+    movie_genre_action.save()
+
+    movie_genre_kids = MovieGenre.objects.create(name="For Kids", url="for-kids")
+    movie_genre_kids.save()
+
+    movie_f1 = Movie.objects.create(
+      title="F1",
+      description="xxx",
+      genre=movie_genre_thriller,
+      image_path="xxx.jpg",
+      url="f1"
+    )
+    movie_f1.save()
+
+    movie_fantastic4 = Movie.objects.create(
+      title="The Fantastic 4: First Steps",
+      description="zzz",
+      genre=movie_genre_thriller,
+      image_path="zzz.jpg",
+      url="fantastic-4"
+    )
+    movie_fantastic4.save()
+
+    movie_falcon_and_winter = Movie.objects.create(
+      title="The Falcon and the Winter Soldier",
+      description="zzz",
+      genre=movie_genre_action,
+      image_path="zzz.jpg",
+      url="falcon-and-winter"
+    )
+    movie_falcon_and_winter.save()
+
+    john_wick_4 = Movie.objects.create(
+      title="John Wick: Chapter 4",
+      description="zzz",
+      genre=movie_genre_action,
+      image_path="zzz.jpg",
+      url="john-wick-4"
+    )
+    john_wick_4.save()
+
+    # ***END Movies***
+
+    # ***Cinema Rooms / Seats***
+    CinemaRoom.objects.all().delete()
+    CinemaRoomRow.objects.all().delete()
+    CinemaRoomSeat.objects.all().delete()
+
+    def generate_seats_for_room(cinema_room: CinemaRoom,
+                                number_of_seats_per_row: int,
+                                number_of_normal_rows: int,
+                                number_of_vip_rows: int,
+                                number_of_normal_seats_after_vip_seats: int,
+                                ticket_type_normal: TicketType,
+                                ticket_type_vip: TicketType,
+                                ) -> None:
+      # Normal rows
+      for row in range(1, number_of_normal_rows + 1):
+        temp_row = CinemaRoomRow.objects.create(
+          row_number=row,
+          cinema_room=cinema_room,
+        )
+
+        for seat in range(1, number_of_seats_per_row + 1):
+          CinemaRoomSeat.objects.create(
+            row=temp_row,
+            seat_number=seat,
+            seat_type=ticket_type_normal,
+          )
+
+      # VIP rows
+      for row in range(number_of_normal_rows + 1, number_of_normal_rows + number_of_vip_rows + 1):
+        temp_row = CinemaRoomRow.objects.create(
+          row_number=row,
+          cinema_room=cinema_room,
+        )
+
+        for seat in range(1, number_of_seats_per_row + 1):
+          CinemaRoomSeat.objects.create(
+            row=temp_row,
+            seat_number=seat,
+            seat_type=ticket_type_vip,
+          )
+
+      # Normal rows
+      for row in range(number_of_normal_rows + number_of_vip_rows + 1,
+                       number_of_normal_rows + number_of_vip_rows + number_of_normal_seats_after_vip_seats + 1):
+        temp_row = CinemaRoomRow.objects.create(
+          row_number=row,
+          cinema_room=cinema_room,
+        )
+
+        for seat in range(1, number_of_seats_per_row + 1):
+          CinemaRoomSeat.objects.create(
+            row=temp_row,
+            seat_number=seat,
+            seat_type=ticket_type_normal
+          )
+
+    room_first = CinemaRoom.objects.create(
+      name="Room 1",
+      cinema=cinema_warsaw_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_first,
+      number_of_seats_per_row=10,
+      number_of_normal_rows=5,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=3,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_second = CinemaRoom.objects.create(
+      name="Room 2",
+      cinema=cinema_warsaw_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_second,
+      number_of_seats_per_row=14,
+      number_of_normal_rows=11,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_third = CinemaRoom.objects.create(
+      name="Room 3",
+      cinema=cinema_warsaw_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_third,
+      number_of_seats_per_row=10,
+      number_of_normal_rows=5,
+      number_of_vip_rows=1,
+      number_of_normal_seats_after_vip_seats=3,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_waw2_first = CinemaRoom.objects.create(
+      name="Room 1",
+      cinema=cinema_warsaw_second,
+    )
+    generate_seats_for_room(
+      cinema_room=room_first,
+      number_of_seats_per_row=10,
+      number_of_normal_rows=6,
+      number_of_vip_rows=1,
+      number_of_normal_seats_after_vip_seats=3,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_waw2_second = CinemaRoom.objects.create(
+      name="Room 2",
+      cinema=cinema_warsaw_second,
+    )
+    generate_seats_for_room(
+      cinema_room=room_second,
+      number_of_seats_per_row=14,
+      number_of_normal_rows=10,
+      number_of_vip_rows=1,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_waw2_third = CinemaRoom.objects.create(
+      name="Room 3",
+      cinema=cinema_warsaw_second,
+    )
+    generate_seats_for_room(
+      cinema_room=room_third,
+      number_of_seats_per_row=10,
+      number_of_normal_rows=4,
+      number_of_vip_rows=3,
+      number_of_normal_seats_after_vip_seats=3,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_fourth = CinemaRoom.objects.create(
+      name="Room 1",
+      cinema=cinema_lublin_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_second,
+      number_of_seats_per_row=14,
+      number_of_normal_rows=11,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_fifth = CinemaRoom.objects.create(
+      name="Room 2",
+      cinema=cinema_lublin_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_fifth,
+      number_of_seats_per_row=9,
+      number_of_normal_rows=7,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_sixth = CinemaRoom.objects.create(
+      name="Room 1",
+      cinema=cinema_krakow_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_sixth,
+      number_of_seats_per_row=14,
+      number_of_normal_rows=11,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    room_seven = CinemaRoom.objects.create(
+      name="Room 2",
+      cinema=cinema_krakow_first,
+    )
+    generate_seats_for_room(
+      cinema_room=room_seven,
+      number_of_seats_per_row=9,
+      number_of_normal_rows=7,
+      number_of_vip_rows=2,
+      number_of_normal_seats_after_vip_seats=4,
+      ticket_type_normal=ticket_type_poland_normal,
+      ticket_type_vip=ticket_type_poland_vip
+    )
+
+    # ***END Cinema Rooms / Seats***
+
+    # ***Showtimes***
+    Showtime.objects.all().delete()
+
+    # ***END Showtimes***
--- backend/movies/models.py
+++ backend/movies/models.py
@@ -9,15 +9,15 @@ Auth Models
 
 
 class Role(models.Model):
-    name = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
 
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, null=True, blank=True)
-    last_name = models.CharField(max_length=100, null=True, blank=True)
-    status = models.IntegerField(default=1)  # 0: banned, 1: active, 2: e-mail not activated
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  role = models.ForeignKey(Role, on_delete=models.CASCADE)
+  first_name = models.CharField(max_length=100, null=True, blank=True)
+  last_name = models.CharField(max_length=100, null=True, blank=True)
+  status = models.IntegerField(default=1)  # 0: banned, 1: active, 2: e-mail not activated
 
 
 '''
@@ -30,23 +30,23 @@ General Models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  url = models.CharField(max_length=100)
 
 
 class City(models.Model):
-    name = models.CharField(max_length=100)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    url = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  country = models.ForeignKey(Country, on_delete=models.CASCADE)
+  url = models.CharField(max_length=100)
 
 
 class TicketType(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
-    currency = models.CharField(max_length=100)
-    primary_ticket = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-    discount_percentage = models.FloatField(null=True, blank=True)
+  country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+  name = models.CharField(max_length=100)
+  price = models.FloatField(null=True, blank=True)
+  currency = models.CharField(max_length=100)
+  primary_ticket = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+  discount_percentage = models.FloatField(null=True, blank=True)
 
 
 '''
@@ -59,30 +59,30 @@ Cinema Models
 
 
 class Cinema(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    postal_code = models.CharField(max_length=20)
-    street = models.CharField(max_length=100)
-    street_number = models.CharField(max_length=20)
-    url = models.CharField(max_length=100)
+  name = models.CharField(max_length=100, null=True, blank=True)
+  city = models.ForeignKey(City, on_delete=models.CASCADE)
+  postal_code = models.CharField(max_length=20)
+  street = models.CharField(max_length=100)
+  street_number = models.CharField(max_length=20)
+  url = models.CharField(max_length=100)
 
 
 class CinemaRoom(models.Model):
-    name = models.CharField(max_length=100)
-    status = models.IntegerField(default=1)  # 0: Broken, 1: Working
-    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  status = models.IntegerField(default=1)  # 0: Broken, 1: Working
+  cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
 
 
 class CinemaRoomRow(models.Model):
-    cinema_room = models.ForeignKey(CinemaRoom, on_delete=models.CASCADE)
-    row_number = models.IntegerField()
+  cinema_room = models.ForeignKey(CinemaRoom, on_delete=models.CASCADE)
+  row_number = models.IntegerField()
 
 
 class CinemaRoomSeat(models.Model):
-    row = models.ForeignKey(CinemaRoomRow, on_delete=models.CASCADE)
-    seat_number = models.IntegerField()
-    status = models.IntegerField(default=1)  # 0: Broken, 1: Working
-    seat_type = models.ForeignKey(TicketType, on_delete=models.SET_NULL, null=True)
+  row = models.ForeignKey(CinemaRoomRow, on_delete=models.CASCADE)
+  seat_number = models.IntegerField()
+  status = models.IntegerField(default=1)  # 0: Broken, 1: Working
+  seat_type = models.ForeignKey(TicketType, on_delete=models.SET_NULL, null=True)
 
 
 '''
@@ -95,23 +95,23 @@ Showtime and Movie Management Models
 
 
 class MovieGenre(models.Model):
-    name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  url = models.CharField(max_length=100)
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    genre = models.ForeignKey(MovieGenre, on_delete=models.SET_NULL, null=True)
-    image_path = models.CharField(max_length=255, null=True, blank=True)
-    url = models.CharField(max_length=255, unique=True)
+  title = models.CharField(max_length=100)
+  description = models.TextField(null=True, blank=True)
+  genre = models.ForeignKey(MovieGenre, on_delete=models.SET_NULL, null=True)
+  image_path = models.CharField(max_length=255, null=True, blank=True)
+  url = models.CharField(max_length=255, unique=True)
 
 
 class Showtime(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    cinema_room = models.ForeignKey(CinemaRoom, on_delete=models.SET_NULL, null=True)
+  movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
+  start_date = models.DateTimeField()
+  end_date = models.DateTimeField()
+  cinema_room = models.ForeignKey(CinemaRoom, on_delete=models.SET_NULL, null=True)
 
 
 '''
@@ -124,24 +124,24 @@ Reservation Managment Models
 
 
 class Reservation(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, blank=True)
-    showtime = models.ForeignKey(Showtime, on_delete=models.SET_NULL, null=True)
-    status = models.IntegerField(default=3)  # 1: Confirmed, 2: Cancelled, 3: In Progress
-    token = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    first_name = models.CharField(max_length=100, null=True, blank=True)  # For customers without account
-    last_name = models.CharField(max_length=100, null=True, blank=True)  # For customers without account
-    email = models.EmailField(null=True, blank=True)  # For customers without account
-    payment_status = models.IntegerField(default=2)  # 1: Paid, 2: Unpaid, 3: Does not require payment, 4: Refunded
-    payment_stripe_id = models.CharField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+  account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, blank=True)
+  showtime = models.ForeignKey(Showtime, on_delete=models.SET_NULL, null=True)
+  status = models.IntegerField(default=3)  # 1: Confirmed, 2: Cancelled, 3: In Progress
+  token = models.CharField(max_length=255, unique=True, null=True, blank=True)
+  first_name = models.CharField(max_length=100, null=True, blank=True)  # For customers without account
+  last_name = models.CharField(max_length=100, null=True, blank=True)  # For customers without account
+  email = models.EmailField(null=True, blank=True)  # For customers without account
+  payment_status = models.IntegerField(default=2)  # 1: Paid, 2: Unpaid, 3: Does not require payment, 4: Refunded
+  payment_stripe_id = models.CharField(max_length=255, null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
 
 class ReservationTicket(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=lib_uuid.uuid4)
-    secret = models.UUIDField(default=lib_uuid.uuid4)
-    reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True, blank=True)
-    cinema_room_seat = models.ForeignKey(CinemaRoomSeat, on_delete=models.SET_NULL, null=True, blank=True)
-    ticket_type = models.ForeignKey(TicketType, on_delete=models.SET_NULL, null=True, blank=True)
+  uuid = models.UUIDField(primary_key=True, default=lib_uuid.uuid4)
+  secret = models.UUIDField(default=lib_uuid.uuid4)
+  reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True, blank=True)
+  cinema_room_seat = models.ForeignKey(CinemaRoomSeat, on_delete=models.SET_NULL, null=True, blank=True)
+  ticket_type = models.ForeignKey(TicketType, on_delete=models.SET_NULL, null=True, blank=True)
 
 
 '''
